daf54dfe4c4d8ee2e19a1b5b482256491993bbc4
ranieriiuri <iuriranierioliveira@gmail.com>
2025-05-23 16:12:08 -0300

    Primeiro commit - backend eclesial arrecadacoes

---
+++ src/main/java/com/ranieriiuri/eclesial_arrecadacoes/EclesialArrecadacoesApplication.java
@@ -0,0 +1,13 @@
+package com.ranieriiuri.eclesial_arrecadacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclesialArrecadacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclesialArrecadacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranieriiuri/eclesial_arrecadacoes/EclesialArrecadacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranieriiuri.eclesial_arrecadacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclesialArrecadacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
