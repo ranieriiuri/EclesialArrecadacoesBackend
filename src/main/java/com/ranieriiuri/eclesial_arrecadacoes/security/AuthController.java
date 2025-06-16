8faa5bbe232586b29e94d70df890bae390704ff9
ranieriiuri <iuriranierioliveira@gmail.com>
2025-06-16 18:26:45 -0300

    AuthController class is created

---
+++ src/main/java/com/ranieriiuri/eclesial_arrecadacoes/security/AuthController.java
@@ -0,0 +1,24 @@
+package com.ranieriiuri.eclesial_arrecadacoes.security;
+
+import com.ranieriiuri.eclesial_arrecadacoes.security.dto.AuthRequest;
+import com.ranieriiuri.eclesial_arrecadacoes.security.dto.AuthResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+// Classe da camada de controle, q recebe a req de login e retorna o token
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
