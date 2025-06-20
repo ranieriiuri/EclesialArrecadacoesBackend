b31a8420c8b847eef7985a9982f8f55f91417851
ranieriiuri <iuriranierioliveira@gmail.com>
2025-06-20 17:51:59 -0300

    swaggerConfig is created

---
+++ src/main/java/com/ranieriiuri/eclesial_arrecadacoes/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.ranieriiuri.eclesial_arrecadacoes.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Eclesial Arrecadações API")
+                        .version("1.0.0")
+                        .description("Documentação da API do sistema de arrecadações e doações."))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
